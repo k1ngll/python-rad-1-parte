8008f97eba362d6aed992df1f165a016b221d081
Sena <66250092+k1ngll@users.noreply.github.com>
2025-03-13 16:45:33 -0300

    Update Visual.py

--- Visual.py
+++ Visual.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk  # Para carregar imagens
+from PIL import Image, ImageTk  
 import os
 class Carro:
     def __init__(self, modelo, preco: float, km_rodados: int, imagem: str):
